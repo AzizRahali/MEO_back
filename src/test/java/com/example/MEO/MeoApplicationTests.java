e836e876d1c7478be0c3ba79928ae89bae7bc96b
AzizRahali <147147915+AzizRahali@users.noreply.github.com>
2024-05-15 12:49:40 +0100

    initial commit

---
+++ src/test/java/com/example/MEO/MeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MEO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
